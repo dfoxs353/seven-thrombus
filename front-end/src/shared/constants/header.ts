7199c2682388c84b3de58b3423730eaf9c8db7ff
Fetisov <rofetisov@mail.ru>
2025-02-04 11:55:07 +0300

    [FRONT] #chore rework routes

---
+++ front-end/src/shared/constants/header.ts
@@ -0,0 +1,14 @@
+import { ROUTES } from "./routes";
+
+export const HEADER = {
+  navLinks: [
+    {
+      title: "Home",
+      href: ROUTES.HOME
+    },
+    {
+      title: "About",
+      href: ROUTES.ABOUT
+    },
+  ]
+}
--- front-end/src/shared/ui/Header/index.tsx
+++ front-end/src/shared/ui/Header/index.tsx
@@ -1,7 +1,20 @@
+import { Link } from "@tanstack/react-router"
+import { HEADER } from "@/shared/constants/header"
+import { Typography } from "../Typography"
+
 export const Header = () => {
     return (
         <header className={'w-screen h-10'}>
-            header
+            <ul className="flex gap-2">
+                {
+                    HEADER.navLinks.map(link =>
+                        <li key={link.title + link.href}>
+                            <Link to={link.href}>
+                                <Typography>{link.title}</Typography>
+                            </Link>
+                        </li>)
+                }
+            </ul>
         </header>
     )
 }
